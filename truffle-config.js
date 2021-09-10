98afe70d082574019de4dd0605ce88a8d36b0a8d
cd33 <charles.diallo@ynov.com>
2021-09-10 12:07:07 +0200

    changes to deploy on heroku and gitpages

--- truffle-config.js
+++ truffle-config.js
@@ -1,64 +1,17 @@
 const path = require("path");
-
-// module.exports = {
-//   // See <http://truffleframework.com/docs/advanced/configuration>
-//   // to customize your Truffle configuration!
-//   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-//   networks: {
-//     development: {
-//       host: "127.0.0.1",
-//       port: 7545,
-//       network_id: 1337,
-//     }
-//   },
-//   compilers: {
-//     solc: {
-//       version: "0.8.0",
-//       settings: {
-//        optimizer: {
-//          enabled: false,
-//          runs: 200
-//        },
-//       evmVersion: "byzantium"
-//       }
-//     }
-//   },
-// };
-
-
 require('dotenv').config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
   networks: {
     contracts_build_directory: path.join(__dirname, "client/src/contracts"),
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "1337",       // Any network (default: none)
-    //  port: 8545,
-    //  network_id: "*",
+     host: "127.0.0.1",
+    //  port: 7545,
+    //  network_id: "1337",
+     port: 8545,
+     network_id: "*",
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/` + process.env.INFURA_ID),
       network_id: 3,       // Ropsten's id
@@ -73,13 +26,6 @@ module.exports = {
       gas: 5500000,
     },
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
